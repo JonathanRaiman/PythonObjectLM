660f4eb9a8abe5f060f4cf58f3b6385d49ae24a1
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-21 23:16:48 -0500

    added a non blas untrainable option, removed theano dep

--- objectlm/__init__.py
+++ objectlm/__init__.py
@@ -1,8 +1,24 @@
-import numpy as np, pyximport, threading, logging, time, sys, os, pickle, gzip, json
+import numpy as np, threading, logging, time, sys, os, pickle, gzip, json
 from collections import OrderedDict
 sys.path.append("/Users/jonathanraiman/Desktop/Coding/language_modeling/")
-pyximport.install(setup_args={"include_dirs": np.get_include()})
-from .train_model import train_sentence_concatenation, predict_sentence_window, predict_distribution_sentence_window
+
+try:
+    import pyximport
+    pyximport.install(setup_args={"include_dirs": np.get_include()})
+    from .train_model import train_sentence_concatenation, predict_sentence_window, predict_distribution_sentence_window
+    training_methods_imported = True
+    err_msg = ""
+except ImportError:
+    err_msg = "Error: Scipy and Cython are required to train the model. To train a model install Scipy and Cython first."
+    training_methods_imported = False
+    print(err_msg)
+    def train_sentence_concatenation(*args):
+        raise NotImplementedError(err_msg)
+    def predict_sentence_window(*args):
+        raise NotImplementedError(err_msg)
+    def predict_distribution_sentence_window(*args):
+        raise NotImplementedError(err_msg)
+
 from gensim import utils as gensim_utils
 from queue import Queue
 from numpy import float32 as REAL, int32 as INT
@@ -24,7 +40,7 @@ try:
         return theano.shared(x, name=name)
 
 except ImportError:
-    print("WARNING: You do not have Theano install, but this is a recommended dependency for ObjectlM")
+    print("WARNING: You do not have Theano installed, but this is a recommended dependency for ObjectlM")
 
     class SharedVariable:
         """
@@ -63,86 +79,82 @@ except ImportError:
 
 class ObjectLM(object):
     """
-
+    
     Object Language Model
     -------------------
-
+    
     Use all sorts of information as a labeled dataset.
-
+    
     Train using language model w/. words
     ------------------------------------
-
+    
     e.g. use an indexing scheme as follows:
-
+    
         'hello' -> 0
         'world' -> 1
         '.'     -> 2
-
+        
     Count the number of words in vocab:
-
+    
         model = ObjectLM(vocabulary_size = 3)
-
+        
     And then train using those words:
-
+    
         model.update_fun(
             [1, 2, 3],
             <index of object>,
             [<labels for multiclass>],
             [<labels for sigmoid>]
         )
-
+    
     Train using indexed objects
     ---------------------------
-
+    
     e.g. use an indexing scheme as follows:
-
+    
         'hello world .' -> 0
         'hello Joe .'   -> 1
-
+        
     Count the number of objects in the object vocabulary:
-
+        
         model = ObjectLM(object_vocabulary_size = 2,
                        vocabulary_size= 1000)
-
+                       
     And then train using those objects:
-
+    
         model.update_fun(
             [<indices of words>],
             0,
             [<labels for multiclass>],
             [<labels for sigmoid>]
         )
-
-
+    
+    
     Train using sigmoid classes
     ---------------------------
-
+    
     e.g. using 6 classes, with classes 3 and 5 active, others off:
-
+    
         model.update_fun(
             [<indices of words>],
             <index of object>,
             [<labels for multiclass>],
             [0, 0, 1, 0, 1, 0])
-
+        
     Train using multiclass labels
     -----------------------------
-
+    
     e.g. using 2 multiclass labels, with 1st label being
     class 3, and 2nd label being class 4:
-
+            
         model.update_fun(
             [<indices of words>],
             <index of object>,
             [2, 3],
             [<labels for sigmoid>]
-
-
-
     """
-
     version_id = 1.0
-
+    
     def load_saved_weights(self, path):
         """
         Replace current model parameters by those
@@ -186,7 +198,7 @@ class ObjectLM(object):
                 self.output_size,
                 projection_size).astype(REAL))
         self.projection_matrix.set_value(proj_matrix)
-
+        
         # in the case we want to use 2nd order behavior for modeling we add a tensor:
         if self.bilinear_form:
             # reset bilinear form matrix:
@@ -195,12 +207,10 @@ class ObjectLM(object):
                 projection_size,
                 projection_size)).astype(REAL)
             self.composition_matrix.set_value(random_comp_matrix)
-
-
+        
         # reset bias vector:
         bias_vector      = np.zeros(self.output_size, dtype=REAL)
         self.bias_vector.set_value(bias_vector)
-
 
         # reset language models:
         model_matrix = ((1.0 / self.size) * \
@@ -210,7 +220,7 @@ class ObjectLM(object):
         object_matrix = ((1.0 / self.object_size) * \
                         np.random.randn(self.object_vocabulary_size, self.object_size)).astype(REAL)
         self.object_matrix.set_value(object_matrix)
-
+            
     def save_model(self, path):
         """
 
@@ -332,26 +342,23 @@ class ObjectLM(object):
         try:
             import scipy.io as scipy_io
             if not path.endswith("/"): path = path + "/"
-
             if not os.path.exists(path): os.makedirs(path)
             param_dict = {}
-
             # if we need to create a normalized version of the word and object vectors
             # we create one now.
-
             for param in self.params:
                 param_dict[param.name] = param.get_value(borrow = True)
 
             scipy_io.savemat(path + "parameters.mat", param_dict)
             self.save_model_parameters(path)
         except ImportError:
-            print("Error, Scipy is need to interact with beautiful land of Java...")
+            print("Error: you need Scipy to output Matlab matrices for Java exports.")
             raise
 
     @property
     def batch_size(self):
         return self._batch_size
-
+    
     def __init__(self,
                  vocabulary,
                  object_vocabulary_size = 100,
@@ -368,7 +375,7 @@ class ObjectLM(object):
                  bilinear_form = False, # not support by cython yet.
                  theano_mode = 'FAST_RUN', # or FAST_COMPILE
                  batch_size = 100):
-
+        
         self.alpha = create_shared(np.float64(alpha).astype(REAL), name = 'alpha')
         self._alpha = float(alpha)
         self.output_classes = np.array(output_classes, dtype=INT)
@@ -386,28 +393,28 @@ class ObjectLM(object):
 
         self.output_sigmoid_labels = output_sigmoid_labels
         self.output_labels = output_labels
-
+        
         self.output_size = output_sigmoid_classes + (sum(output_classes) if len(output_classes) > 0 else 0)
-
+        
         self.params = []
         self.indexed_params = []
-
+        
         self.create_shared_variables()
         if create_theano_functions:
             self.create_theano_functions()
-
+    
     def create_shared_variables(self):
         """
         Create the theano variables:
         ----------------------------
-
+        
         1. the linear form taking all the words in a window, and
         2. the bilinear form tensor
         3. adding a bias vector, to get an output class prediction of 'output_size'
            size.
-
+           
         """
-
+        
         # in the typical case we consider a linear projection of the concatenation
         # or addition of inputs:
         if self.concatenate:
@@ -416,16 +423,16 @@ class ObjectLM(object):
             assert(self.size == self.object_size), "In additive (not concatenated) projection, "
             "the hidden objects must have the same dimensionality as the language model vectors."
             projection_size = self.size
-
-
+            
+        
         # create linear form matrix:
         proj_matrix = ((1.0 / self.size) * \
             np.random.randn(
                 self.output_size,
                 projection_size).astype(REAL))
-        self.projection_matrix = create_shared(proj_matrix, name='projection_matrix')
+        self.projection_matrix = create_shared(proj_matrix, name='projection_matrix')    
         self.params.append(self.projection_matrix)
-
+        
         # in the case we want to use 2nd order behavior for modeling we add a tensor:
         if self.bilinear_form:
             # create bilinear form matrix:
@@ -434,28 +441,28 @@ class ObjectLM(object):
                 projection_size,
                 projection_size)).astype(REAL)
             self.composition_matrix = create_shared(random_comp_matrix, name='composition_matrix')
-
+            
             self.params.append(self.composition_matrix)
-
-
-
+        
+        
+        
         # create bias vector:
         bias_vector      = np.zeros(self.output_size, dtype=REAL)
         self.bias_vector = create_shared(bias_vector, name='bias_vector')
         self.params.append(self.bias_vector)
-
+        
         model_matrix = ((1.0 / self.size) * \
                         np.random.randn(self.vocabulary_size, self.size)).astype(REAL)
         self.model_matrix = create_shared(model_matrix, name='model_matrix')
         self.params.append(self.model_matrix)
-
+        
         self.indexed_params.append(self.model_matrix)
-
+        
         object_matrix = ((1.0 / self.object_size) * \
                         np.random.randn(self.object_vocabulary_size, self.object_size)).astype(REAL)
         self.object_matrix = create_shared(object_matrix, name='object_matrix')
         self.params.append(self.object_matrix)
-
+        
         self.indexed_params.append(self.object_matrix)
 
     def create_normalized_matrices(self):
@@ -504,48 +511,48 @@ class ObjectLM(object):
             log_unnormalized_pred = theano_bilinear_form(self.composition_matrix, observation) + T.dot(self.projection_matrix, observation) + self.bias_vector
         else:
             log_unnormalized_pred = T.dot(self.projection_matrix, observation)  + self.bias_vector
-
+        
         prev_position = 0
         preds = []
-
+        
         error = 0.0
         for i, class_size in enumerate(self.output_classes):
             class_prediction = T.nnet.softmax(log_unnormalized_pred[prev_position:prev_position + class_size])
             preds.append(class_prediction[0])
             prev_position += class_size
             error += T.nnet.categorical_crossentropy(class_prediction, labels[i:i+1]).sum()
-
+        
         prediction = T.nnet.sigmoid(log_unnormalized_pred[prev_position:])
         error += T.nnet.binary_crossentropy(prediction, sigmoid_labels).sum()
 
         return (preds, prediction, error)
-
+    
     def create_theano_functions(self):
         """
             Given examples of the form:
-
+                
                 (
                     [1100, 1200, 12],
                     [1, 2, 0, 0, 1, 0]
                 )
-
+                
             Corresponding to a sequence of words 1100, 1200 and an object 12,
             with labels for class 1, 1, class 2, 2, and for the sigmoid classes
             the third class active, we can do regression.
-
+            
         """
         try:
 
             import theano, theano.tensor as T
-
+            
             input = T.ivector('input')
             input_object = T.iscalar('input_object_index')
             labels = T.ivector('labels')
             sigmoid_labels = T.ivector('sigmoid_labels')
-
+            
             embeddings = self.model_matrix[input]
             object_embedding = self.object_matrix[input_object]
-
+            
             if self.concatenate:
                 # or we concatenate all the words and add the object to it
                 merged_embeddings = T.concatenate([embeddings.ravel(), object_embedding])
@@ -556,9 +563,9 @@ class ObjectLM(object):
             preds, prediction, error = self.projection_fun(merged_embeddings, labels, sigmoid_labels)
 
             updates = OrderedDict()
-
+            
             gparams = T.grad(error, self.params)
-
+            
             for gparam, param in zip(gparams, self.params):
                 if param == self.model_matrix:
                     updates[param] = T.inc_subtensor(param[input], - self.alpha * gparam[input])
@@ -566,7 +573,7 @@ class ObjectLM(object):
                     updates[param] = T.inc_subtensor(param[input_object], - self.alpha * gparam[input_object])
                 else:
                     updates[param] = param - self.alpha * gparam
-
+                
             self.predict_proba = theano.function([input, input_object], preds + [prediction], mode = self.theano_mode)
             self.predict = theano.function([input, input_object], [pred.argmax() for pred in preds] + [prediction.round()], mode = self.theano_mode)
 
@@ -575,13 +582,13 @@ class ObjectLM(object):
             self.predict_vector = theano.function([input_vector], [pred.argmax() for pred in alt_preds] + [alt_prediction.round()], mode = self.theano_mode)
 
             self.predict_vector_proba = theano.function([input_vector], alt_preds + [alt_prediction], mode = self.theano_mode)
-
+            
             training_inputs = []
             if len(self.output_classes) > 0:
                 training_inputs.append(labels)
             if self.output_sigmoid_classes > 0:
                 training_inputs.append(sigmoid_labels)
-
+            
             self.gradient_fun  = theano.function([input, input_object] + training_inputs, gparams, mode = self.theano_mode)
             self.update_fun    = theano.function([input, input_object] + training_inputs, error, updates = updates, mode = self.theano_mode)
         except ImportError:
@@ -600,6 +607,8 @@ class ObjectLM(object):
         Each sentence must be a list of utf8 strings.
 
         """
+        if not training_methods_imported:
+            raise NotImplementedError(err_msg)
         logger.info("training model with %i workers" % (workers))
 
         start, next_report = time.time(), [1.0]
@@ -657,25 +666,25 @@ class CategoriesConverter(object):
     """
     Sorts and converts named lists into
     binary vectors.
-
+    
     """
     def __init__(self, categories):
         self.num_categories = len(categories)
         self.index2category = list(categories)
-
+        
         vocab = {}
         for i, cat in enumerate(self.index2category):
             vocab[cat] = i
         self.category2index = vocab
-
+        
     def convert_categories_to_label(self, categories):
         label = np.zeros(self.num_categories, dtype=INT)
-
+        
         for cat in categories:
             label[self.category2index[cat]] = 1
-
-        return label
-
+            
+        return label        
+    
     def save(self, path):
         with gzip.open(path, 'wb') as file:
             pickle.dump(self, file, 1)
@@ -684,7 +693,7 @@ class CategoriesConverter(object):
         with gzip.open(path, 'wt') as file:
             for category in self.index2category:
                 file.write((category + "\n"))
-
+            
     @staticmethod
     def load(path):
         file = gzip.open(path, 'r')
@@ -707,10 +716,10 @@ class DatasetGenerator():
         with gzip.open(path, "wt") as f:
             json.dump(self.texts_data, f)
 
-
+        
     def convert_datum_to_classes(self, datum):
         return np.array([len(datum["price"]), max(0, int(datum["rating"])-1)], dtype=INT)
-
+    
     def __iter__(self):
         for i, text in enumerate(self.texts):
             yield((text,
